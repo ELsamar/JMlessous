ad35c47e81bc47353927cf6897bfcabf7f6793bb
marouabilel <maroua.bilel@esprit.tn>
2021-01-01 18:42:13 +0100

    EpargneBack

--- src/main/java/com/jaune/JMlessous/model/CompteEpargne.java
+++ src/main/java/com/jaune/JMlessous/model/CompteEpargne.java
@@ -7,14 +7,67 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.util.Date;
+
 @Entity
 @Table(name="compteepargne")
 
 public class CompteEpargne {
 	 @Id
 	  @GeneratedValue(strategy=GenerationType.IDENTITY)
-	  private int  id;
-	@ManyToOne
-	
+	 private int  id;
+	 private int  RIB;
+	 private int  plafond;
+	 private float  solde;
+     private Date dateOuverture;
+
+	 @ManyToOne
 	private Client client;
+	
+	
+	
+	
+	
+	/*********************/
+	 public CompteEpargne() {
+			super();
+		}
+	 public CompteEpargne(Client Client, Date dateOuverture,int RIB,int plafond,float solde) {
+			super();
+			this.Client = Client;
+			this.RIB = RIB;
+			this.dateOuverture = dateOuverture;
+			this.plafond = plafond;
+			this.solde=solde;
+		}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRIB() {
+		return RIB;
+	}
+	public void setRIB(int RIB) {
+		this.RIB = RIB;
+	}
+	public float getSolde() {
+		return solde;
+	}
+	public void setSolde(float solde) {
+		this.solde = solde;
+	}
+	public int getPlafond() {
+		return plafond;
+	}
+	public void setPlafond(int plafond) {
+		this.plafond = plafond;
+	}
+	public Date getDateOuverture() {
+		return dateOuverture;
+	}
+	public void setDateOuverture (Date dateOuverture) {
+		this.dateOuverture = dateOuverture;
+	}
 }
